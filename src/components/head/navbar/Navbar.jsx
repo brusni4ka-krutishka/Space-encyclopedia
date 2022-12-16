1a1662dc2d4d53ce2c2f1c00486285b857f2c286
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-12-16 23:20:39 +0300

    links to pages added

--- src/components/head/navbar/Navbar.jsx
+++ src/components/head/navbar/Navbar.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ss from './navbar.module.css';
 
 export default function Navbar() {
   return (
     <ul className={ss.navbar__list}>
-      <li>Главная</li>
-      <li>Космос</li>
-      <li>Галерея</li>
-      <li>О нас</li>
+      <li>
+        <Link to={'/'}>Главная</Link>
+      </li>
+      <li>
+        <Link to={'/cosmos'}>Космос</Link>
+      </li>
+      <li>
+        <Link to={'/gallery'}>Галерея</Link>
+      </li>
+      <li>
+        <Link to={'/about'}>О нас</Link>
+      </li>
     </ul>
   );
 }
