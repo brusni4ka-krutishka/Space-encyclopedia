9d337aa195a9ba105f5942234203ec48c0502e28
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-12-17 16:49:32 +0300

    Planet set updated

--- src/data/Planetset.js
+++ src/data/Planetset.js
@@ -1,8 +1,43 @@
-import earth from '../assets/images/cosmosPage/planets/earth.png';
+import Earth from '../assets/images/cosmosPage/planets/earth.png';
+import Jupiter from '../assets/images/cosmosPage/planets/Jupiter.png';
+import Mars from '../assets/images/cosmosPage/planets/Mars.png';
+import Mercury from '../assets/images/cosmosPage/planets/Mercury.png';
+import Neptune from '../assets/images/cosmosPage/planets/Neptune.png';
+import Saturn from '../assets/images/cosmosPage/planets/Saturn.png';
+import Uranus from '../assets/images/cosmosPage/planets/Uranus.png';
+import Venus from '../assets/images/cosmosPage/planets/Venus.png';
 
 export const planetSet = [
   {
-    img: earth,
+    img: Earth,
     undertext: 'Земля',
+  },
+  {
+    img: Jupiter,
+    undertext: 'Юпитер',
+  },
+  {
+    img: Mars,
+    undertext: 'Марс',
+  },
+  {
+    img: Mercury,
+    undertext: 'Меркурий',
+  },
+  {
+    img: Neptune,
+    undertext: 'Нептун',
+  },
+  {
+    img: Saturn,
+    undertext: 'Сатурн',
+  },
+  {
+    img: Uranus,
+    undertext: 'Уран',
+  },
+  {
+    img: Venus,
+    undertext: 'Венера',
   },
 ];
