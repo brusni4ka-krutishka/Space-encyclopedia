9c0775cd14ab2681c481795af6d0fdecec306904
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-12-17 15:44:16 +0300

    Startset data added

---
+++ src/data/Starset.js
@@ -0,0 +1,33 @@
+import sun from '../assets/images/cosmosPage/stars/Sun.png';
+import zetaAft from '../assets/images/cosmosPage/stars/Zeta_aft.png';
+import sirius from '../assets/images/cosmosPage/stars/Sirius.png';
+import betelgeuse from '../assets/images/cosmosPage/stars/Betelgeuse.png';
+import altair from '../assets/images/cosmosPage/stars/Altair.png';
+import acrux from '../assets/images/cosmosPage/stars/Acrux.png';
+
+export const starSet = [
+  {
+    img: sun,
+    undertext: 'Солнце',
+  },
+  {
+    img: sirius,
+    undertext: 'Сириус',
+  },
+  {
+    img: betelgeuse,
+    undertext: 'Бетельгейзе',
+  },
+  {
+    img: zetaAft,
+    undertext: 'Дзета кормы',
+  },
+  {
+    img: altair,
+    undertext: 'Альтаир',
+  },
+  {
+    img: acrux,
+    undertext: 'Акрукс',
+  },
+];
