6361da45ce9d4200c34107348cb48c78dd162dfb
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-12-17 16:03:55 +0300

    Little planets data set added

---
+++ src/data/LittlePlanetsset.js
@@ -0,0 +1,8 @@
+import littlePlanet from '../assets/images/cosmosPage/littlePlanets/littlePlanet.png';
+
+export const littlePlanetSet = [
+  {
+    img: littlePlanet,
+    undertext: 'Какая-то планета',
+  },
+];
