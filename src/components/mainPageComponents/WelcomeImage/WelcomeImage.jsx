73651e681bc24ee984a0da0ba0a7aa760773efa5
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-12-17 11:14:19 +0300

    Hello text added to main page

--- src/components/mainPageComponents/WelcomeImage/WelcomeImage.jsx
+++ src/components/mainPageComponents/WelcomeImage/WelcomeImage.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import welcomeImage from '../../../assets/images/mainPage/welcome.png';
 import ss from './welcomeImage.module.css';
+
 export default function WelcomeImage() {
-  return <img src={welcomeImage} alt='hello image' className={ss.image} />;
+  return (
+    <div className={ss.container}>
+      <div className={ss.text_block}>
+        <h1 className={ss.text}>
+          Добро пожаловать в <b className={ss.decor}> Space Encyclopedia!</b>
+        </h1>
+        <h2 className={ss.text}>
+          Вы откроете для себя много <b>нового</b> и <b>интересного</b>
+        </h2>
+      </div>
+      <img src={welcomeImage} alt='hello image' className={ss.image} />
+    </div>
+  );
 }
