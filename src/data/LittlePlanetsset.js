321db84d0c24f378a2053e9a86b08fbee982622a
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-12-17 16:38:04 +0300

    LittlePlanetsset updated

--- src/data/LittlePlanetsset.js
+++ src/data/LittlePlanetsset.js
@@ -1,8 +1,33 @@
-import littlePlanet from '../assets/images/cosmosPage/littlePlanets/littlePlanet.png';
+import Pluto from '../assets/images/cosmosPage/littlePlanets/littlePlanet.png';
+import Ceres from '../assets/images/cosmosPage/littlePlanets/Ceres.png';
+import Erida from '../assets/images/cosmosPage/littlePlanets/Erida.png';
+import Jaumea from '../assets/images/cosmosPage/littlePlanets/Jaumea.png';
+import Makemake from '../assets/images/cosmosPage/littlePlanets/Makemake.png';
+import Sedna from '../assets/images/cosmosPage/littlePlanets/Sedna.png';
 
 export const littlePlanetSet = [
   {
-    img: littlePlanet,
-    undertext: 'Какая-то планета',
+    img: Pluto,
+    undertext: 'Плутон',
+  },
+  {
+    img: Ceres,
+    undertext: 'Церера',
+  },
+  {
+    img: Jaumea,
+    undertext: 'Хаумеа',
+  },
+  {
+    img: Erida,
+    undertext: 'Эрида',
+  },
+  {
+    img: Makemake,
+    undertext: 'Макемаке',
+  },
+  {
+    img: Sedna,
+    undertext: 'Седна',
   },
 ];
