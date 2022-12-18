2bd4e9e5de595075a0cbb6a68e82b21802bafb5e
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-12-18 22:32:19 +0300

    bg added

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import SpaceObjectsPhotoContainer from '../components/galleryPageComponents/SpaceObjectsPhotoContainer/SpaceObjectsPhotoContainer.jsx';
 import { GallerySet } from '../data/Galleryset.js';
-
+import bg from '../assets/images/galleryPage/bg.png';
 export default function GalleryPage() {
-  return <SpaceObjectsPhotoContainer objectsSet={GallerySet} />;
+  return (
+    <div style={{ backgroundImage: `url(${bg})` }}>
+      <h1 style={{ textAlign: 'center' }}>Звезды</h1>
+      <SpaceObjectsPhotoContainer objectsSet={GallerySet} />
+    </div>
+  );
 }
