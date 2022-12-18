4d5d2c6ca698b3b1c6c95d3fddb90c03234c9281
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-12-18 21:48:02 +0300

    gallery base

---
+++ src/components/galleryPageComponents/SpaceObjectPhoto/SpaceObjectPhoto.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ss from './SpaceObjectPhoto.module.css';
+export default function SpecificSpaceObject({
+  img,
+  undertext,
+  alt = 'Space Object',
+}) {
+  return (
+    <>
+      <div
+        className={ss.object_container}
+        onClick={(e) => {
+          if (
+            e.target.className === ss.object_container ||
+            e.target.className === ss.img ||
+            e.target.className === ss.undertext
+          ) {
+            setIsModalOpen(true);
+          }
+        }}
+      >
+        <img className={ss.img} src={img} alt={alt} />
+        <h2 className={ss.undertext}>{undertext}</h2>
+      </div>
+    </>
+  );
+}
---
+++ src/components/galleryPageComponents/SpaceObjectsPhotoContainer/SpaceObjectsPhotoContainer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ss from './SpaceObjectsPhotoContainer.module.css';
+import SpecificSpaceObject from '../SpaceObjectsPhotoContainer/SpaceObjectsPhotoContainer.jsx';
+
+export default function SpecificSpaceObjectsContainer({ objectsSet }) {
+  return (
+    <div className={ss.container}>
+      {objectsSet.map((object) => (
+        <SpecificSpaceObject
+          img={object.img}
+          undertext={object.undertext}
+          key={object.img}
+          fact={object.fact}
+          description={object.description}
+          characteristicsList={object.characteristicsList}
+          intrestingFactsList={object.intrestingFactsList}
+        />
+      ))}
+    </div>
+  );
+}
