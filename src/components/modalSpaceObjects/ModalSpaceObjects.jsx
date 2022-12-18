75e09c087df0f7acffd7a9c90a6c7248edbd3eba
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-12-18 11:52:36 +0300

    modal parameters added

--- src/components/modalSpaceObjects/ModalSpaceObjects.jsx
+++ src/components/modalSpaceObjects/ModalSpaceObjects.jsx
@@ -1,68 +1,46 @@
 import React from 'react';
 import ss from './modalSpaceObjects.module.css';
-import sun from '../../assets/images/cosmosPage/stars/Sun.png';
-export default function ModalSpaceObjects() {
+export default function ModalSpaceObjects({
+  objectName,
+  img,
+  fact,
+  characteristicsList,
+  description,
+  intrestingFactsList,
+}) {
   return (
     <div className={ss.container}>
       <div id={ss.modal}>
-        <h1 className={ss.modal_header}>Седна</h1>
+        <h1 className={ss.modal_header}>{objectName}</h1>
         <div className={ss.modal_body}>
           <div className={ss.img_and_text}>
-            <img className={ss.modal_body_left_img} src={sun} />
+            <img className={ss.modal_body_left_img} src={img} alt={'obj'} />
             <div className={ss.modal_body_right_text}>
-              <h3 className={ss.right_text_head}> Заголовок</h3>
+              <h3 className={ss.right_text_head}>{fact}</h3>
               <div className={ss.right_text_body}>
-                <p className={ss.key}>Диаметр</p>
-                <p className={ss.value}>120 536 км</p>
+                {characteristicsList.map((item) => {
+                  return (
+                    <>
+                      <p className={ss.key}>{item['key']}</p>
+                      <p className={ss.value}>{item['value']}</p>
+                    </>
+                  );
+                })}
               </div>
             </div>
           </div>
           <div className={ss.modal_body_bottom_text}>
-            Здесь будет охуенный текст-описание планеты Lorem ipsum dolor sit,
-            amet consectetur adipisicing elit. Quae consectetur veniam
-            necessitatibus impedit numquam unde dolore iste accusamus maxime
-            eius soluta id, magni, dolorum modi! Eveniet enim cupiditate nemo
-            soluta. Здесь будет охуенный текст-описание планеты Lorem ipsum
-            dolor sit, amet consectetur adipisicing elit. Quae consectetur
-            veniam necessitatibus impedit numquam unde dolore iste accusamus
-            maxime eius soluta id, magni, dolorum modi! Eveniet enim cupiditate
-            nemo soluta. Здесь будет охуенный текст-описание планеты Lorem ipsum
-            dolor sit, amet consectetur adipisicing elit. Quae consectetur
-            veniam necessitatibus impedit numquam unde dolore iste accusamus
-            maxime eius soluta id, magni, dolorum modi! Eveniet enim cupiditate
-            nemo soluta. Здесь будет охуенный текст-описание планеты Lorem ipsum
-            dolor sit, amet consectetur adipisicing elit. Quae consectetur
-            veniam necessitatibus impedit numquam unde dolore iste accusamus
-            maxime eius soluta id, magni, dolorum modi! Eveniet enim cupiditate
-            nemo soluta. Здесь будет охуенный текст-описание планеты Lorem ipsum
-            dolor sit, amet consectetur adipisicing elit. Quae consectetur
-            veniam necessitatibus impedit numquam unde dolore iste accusamus
-            maxime eius soluta id, magni, dolorum modi! Eveniet enim cupiditate
-            nemo soluta. Здесь будет охуенный текст-описание планеты Lorem ipsum
-            dolor sit, amet consectetur adipisicing elit. Quae consectetur
-            veniam necessitatibus impedit numquam unde dolore iste accusamus
-            maxime eius soluta id, magni, dolorum modi! Eveniet enim cupiditate
-            nemo soluta. Здесь будет охуенный текст-описание планеты Lorem ipsum
-            dolor sit, amet consectetur adipisicing elit. Quae consectetur
-            veniam necessitatibus impedit numquam unde dolore iste accusamus
-            maxime eius soluta id, magni, dolorum modi! Eveniet enim cupiditate
-            nemo soluta. Здесь будет охуенный текст-описание планеты Lorem ipsum
-            dolor sit, amet consectetur adipisicing elit. Quae consectetur
-            veniam necessitatibus impedit numquam unde dolore iste accusamus
-            maxime eius soluta id, magni, dolorum modi! Eveniet enim cupiditate
-            nemo soluta. Здесь будет охуенный текст-описание планеты Lorem ipsum
-            dolor sit, amet consectetur adipisicing elit. Quae consectetur
-            veniam necessitatibus impedit numquam unde dolore iste accusamus
-            maxime eius soluta id, magni, dolorum modi! Eveniet enim cupiditate
-            nemo soluta. Здесь будет охуенный текст-описание планеты Lorem ipsum
-            dolor sit, amet consectetur adipisicing elit. Quae consectetur
-            veniam necessitatibus impedit numquam unde dolore iste accusamus
-            maxime eius soluta id, magni, dolorum modi! Eveniet enim cupiditate
-            nemo soluta.
+            {description}
             <h2>Сведения и факты о небесном теле</h2>
             <div className={ss.list}>
-              <div className={ss.list_item_num}>1</div>
-              <div className={ss.list_item_text}>Lfksjdlkfjlk</div>
+              {intrestingFactsList.map((item, i) => {
+                return (
+                  <>
+                    <div className={ss.list_item_num}>{i + 1}</div>
+                    <div className={ss.list_item_text}>{item}</div>
+                  </>
+                );
+              })}
             </div>
           </div>
         </div>
