97d63815b8470a44fc08c5ff58639edd3f99449e
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-12-18 21:53:39 +0300

    gallery set added

---
+++ src/data/Galleryset.js
@@ -0,0 +1,18 @@
+import first from '../assets/images/galleryPage/1.png';
+import second from '../assets/images/galleryPage/2.png';
+import third from '../assets/images/galleryPage/3.png';
+
+export const GallerySet = [
+  {
+    img: first,
+    undertext: 'Столб звездообразования',
+  },
+  {
+    img: second,
+    undertext: 'Звёздное скопление Семи сестёр',
+  },
+  {
+    img: third,
+    undertext: 'Объединение пары галактик',
+  },
+];
